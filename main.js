2be853bbb1c9fd07206f46c4ecaadc7461888444
Brooks Van Buren <woofiewilly@gmail.com>
2016-03-04 15:20:32 -0500

    Elaborated on some comments, preparing to add collision detection
    I elaborated on some of the comments so they are more educational if we
    look at them later on. I also put the drawing algorithm inside a draw
    loop, and set it to an interval. I also took out the radius from the
    ball drawing arguments, and replaced it with the variable ballradius.
    More variables will have to be added in order to handle collisions,
    including position x and y, and velocity x and y.

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ var canvas = document.querySelector('canvas');
 // Allows us to manipulate canvas
 var ctx = canvas.getContext("2d");
 
+// Variable for ball radius
+var ballradius = 40;
+
 // Draw a ball that acts as a shooter. We'll worry about graphics later.
 // This will have to be put into a function
 ctx.beginPath();
-// x-axis, y-axis, radius, starting angle, and ending angle
-ctx.arc(150, 150, 40, 0, Math.PI*2);
+// x-axis, y-axis, radius, starting angle, ending angle, direction to measure
+// angle (false for clockwise (default), true for counterclockwise)
+ctx.arc(150, 150, ballradius, 0, Math.PI*2, false);
 // Color of circle
 ctx.fillStyle = "#9b59b6";
 // Texts the ctx to actually draw the circle on the canvas
 ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(200, 300, 0, Math.PI, true);
+ctx.strokeStyle = "yellow";
+ctx.stroke();
 ctx.closePath();
