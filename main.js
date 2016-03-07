0a457037f1f76b77d9041c352dbf7a2388b10329
Dan Zheng <zheng321@purdue.edu>
2016-03-07 18:42:05 -0500

    Add drawShooter() and shooter.updateGravity() to main.js

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ var b = 150;
 var da = 2;
 var db = 2;
 
-// Variable for ball radius
-var ballradius = 40;
 
 // We should use an array to keep track of all enemies and their info
 var enemies = [];
@@ -21,10 +19,12 @@ var bullets = [];
 
 // Object containing information about shooter
 var shooter = {
+  // Radius for shooter
+  radius : 40,
   // Position on canvas
   position : {
-    x:0,
-    y:0
+    x:150,
+    y:150
   },
   // Current velocity on canvas
   velocity : {
@@ -37,12 +37,15 @@ var shooter = {
   velocity_cap : 20,
   // Direction shooter is facing, in radians
   direction : 0,
+  // Acceleration downwards due to gravity
+  gravity : 0.05,
 
   // Function that updates direction field each frame
   /* NOTE: this function will have to be changed in order to handle the event
       that only one of velocity.y or velocity.x is 0
   */
-  update_direction : function() {
+  updateDirection : function() {
+    var velocity = this.velocity;
     // If velocities are zero, don't change direction
     if (velocity.y == 0 && velocity.x == 0) {
       return;
@@ -56,8 +59,7 @@ var shooter = {
         // Shift around unit circle to quadrant 4
         direction = angle + Math.PI * 3 / 2;
         return;
-      }
-      else if (velocity.x < 0){ // Must be going left
+      } else if (velocity.x < 0) { // Must be going left
         // Shift to quadrant 3
         direction = angle + Math.PI;
         return;
@@ -70,32 +72,54 @@ var shooter = {
         // No shift needed
         direction = angle;
         return;
-      }
-      else if (velocity.x < 0) { // Must be going left
+      } else if (velocity.x < 0) { // Must be going left
         // Shift to quadrant 2
         direction = angle + Math.PI / 2;
         return;
       }
     }
+  },
+
+  updateGravity : function() {
+
+      var velocity = this.velocity;
+      // Gravity
+      velocity.y += this.gravity;
+      this.position.y += velocity.y;
+
+      var floor = canvas.height - this.radius;
+      if (this.position.y > floor) {
+          this.position.y = floor;
+      }
   }
 }
 
-  function draw() {
-      //Clear the pass of the trail
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      // Draw a ball that acts as a shooter. We'll worry about graphics later.
-      // This will have to be put into a function
-      ctx.beginPath();
-      // x-axis, y-axis, radius, starting angle, and ending angle
-      ctx.arc(a, b, 40, 0, Math.PI*2);
-      // Color of circle
-      ctx.fillStyle = "#9b59b6";
-      // Texts the ctx to actually draw the circle on the canvas
-      ctx.fill();
-      ctx.closePath();
-      //update variable of a & b location
-      a += da;
-      b += db
-  }
+function drawShooter() {
+    // Draw a ball that acts as a shooter. We'll worry about graphics later.
+    // This will have to be put into a function
+    ctx.beginPath();
+
+    // Create new shooter
+    var s = shooter;
+    // x-axis, y-axis, radius, starting angle, and ending angle
+    ctx.arc(s.position.x, s.position.y, s.radius, 0, Math.PI*2);
+
+    // Color of circle
+    ctx.fillStyle = "#9b59b6";
+    // Texts the ctx to actually draw the circle on the canvas
+    ctx.fill();
+    ctx.closePath();
+    s.updateDirection();
+    s.updateGravity();
+}
+
+function draw() {
+    //Clear the pass of the trail
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //drawBall();
+
+    drawShooter();
+}
+
 // Repeat function draw every 10 ms
 setInterval(draw, 10);
